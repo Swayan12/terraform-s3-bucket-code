66643d0f2e8a6d585c3e235c7af1787e6cdec595
Swayan <134930488+Swayan12@users.noreply.github.com>
2023-07-14 11:50:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
   region     = "us-east-2"
-  access_key = ""
-  secret_key = ""
+  access_key = "AKIAXSUTXZMG3UECPCWN"
+  secret_key = "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"
 }
 
 #1 -this will create a S3 bucket in AWS
 resource "aws_s3_bucket" "terraform_state_s3" {
   #make sure you give unique bucket name
-  bucket = "terraform-teknobucket1-state" 
+  bucket = "terraform-swayan1-state" 
   force_destroy = true
 # Enable versioning to see full revision history of our state files
   versioning {
